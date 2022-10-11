d2a149f8cf7524e7b39ac96e501215a8f1178b18
yaoliu1212 <liuyao201212@gmail.com>
2022-10-10 20:26:23 -0400

    valid Header component

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDGRREoDTJSUkHYe8L1js6llMey59UVMY8",
+    authDomain: "clone-99159.firebaseapp.com",
+    projectId: "clone-99159",
+    storageBucket: "clone-99159.appspot.com",
+    messagingSenderId: "989746813126",
+    appId: "1:989746813126:web:fc77621dcd158fc7665fc7",
+    measurementId: "G-EWP41Z4YCR"
+};
